e52991b5a2780ea08b79918d0ea418a4929d3a0d
nguyenvantui <38522004+nguyenvantui@users.noreply.github.com>
2019-05-14 14:39:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from ipdb import set_trace as bp
+
 import time
 import torch.nn as nn
-import  torch.optim as optim
+import torch.optim as optim
 from torchvision import datasets, transforms
 import os
 import torch
@@ -14,17 +16,21 @@ from matplotlib.pyplot import imshow
 from config import *
 import torch.nn.functional as F
 import torch
-from ipdb import set_trace as bp 
+from tqdm import tqdm
 
 # look at my code my code is amazing
 # slow and steady
 # criterion = nn.CrossEntropyLoss()
+# bp()
+# kill or be kill
 
-batch_size = 128
+batch_size = 80
 model_name = "resnet18"
-net = qmodel(model_name ,"cuda")
+net = qmodel(model_name, "cuda")
 
-train_transforms = transforms.Compose([transforms.ToTensor()
+print(">>>>>>>> Start <<<<<<<<<<")
+train_transforms = transforms.Compose([transforms.Resize((64, 64)),
+                                       transforms.ToTensor()
                                        ])
 
 test_transforms = transforms.Compose([transforms.ToTensor()
@@ -40,16 +46,22 @@ datadir_test = datadir + "test"
 # xx = train_data[1]
 
 best_acc = 0
+real_best_acc = 0
+# nhan chi so tinh bon thien
 
 link_temp = "./checkpoint/*.pt"
-link_temp = link_temp.replace("*",model_name)
+real_link_temp = "./checkpoint/real_*.pt"
+link_temp = link_temp.replace("*", model_name)
+real_link_temp = real_link_temp.replace("*", model_name)
 print(link_temp)
-if os.path.isfile(link_temp)==False:
+print(real_link_temp)
+
+if os.path.isfile(link_temp) == False:
     state = {
-            'net': net.state_dict(),
-            'acc': 0,
-            'epoch': 0,
-        }
+        'net': net.state_dict(),
+        'acc': 0,
+        'epoch': 0,
+    }
     start_epoch = state['epoch']
     best_acc = state['acc']
 
@@ -63,21 +75,28 @@ else:
     best_acc = checkpoint['acc']
     start_epoch = checkpoint['epoch']
 
-    
+if os.path.isfile(real_link_temp) == True:
+    real_checkpoint = torch.load(real_link_temp)
+    real_best_acc = checkpoint['acc']
+
 # print(datadir_train)
 # print(datadir_test)
 
 # train_data = datasets.ImageFolder(datadir_train, transform=train_transforms)
 
+real_test_data = datasets.ImageFolder("data/test", transform=test_transforms)
+
 train_load = torch.utils.data.DataLoader(dataset=train_data,
                                          batch_size=batch_size,
-                                         shuffle=True,
-                                         num_workers=10)
+                                         shuffle=True)
 
 test_load = torch.utils.data.DataLoader(dataset=test_data,
                                         batch_size=batch_size,
-                                        shuffle=False,
-                                        num_workers=10)
+                                        shuffle=False)
+
+real_test_load = torch.utils.data.DataLoader(dataset=real_test_data,
+                                        batch_size=batch_size,
+                                        shuffle=False)
 
 
 def lr_decay(epoch):
@@ -86,19 +105,19 @@ def lr_decay(epoch):
     # 0.01 for epoch from 150,250
     # 0.001 for epoch [250,350)
 
-    if epoch<1:
+    if epoch < 1:
         return 0.3
 
-    if epoch<3:
+    if epoch < 3:
         return 0.2
 
-    if epoch<150:
+    if epoch < 150:
         return 0.1
 
-    if epoch<250:
+    if epoch < 250:
         return 0.001
 
-    if epoch<350:
+    if epoch < 350:
         return 0.0001
 
     return 0.00001
@@ -107,15 +126,17 @@ def lr_decay(epoch):
 #     for batch_idx, (inputs, targets) in enumerate(train_load):
 #         pass
 
+
 def train(epoch):
 
     device = "cuda"
     _lr = lr_decay(epoch)
     net.train()
 
-    print("Learning_rate: ",_lr)
+    print("Learning_rate: ", _lr)
     # if optim_method == "SGD":
-    optimizer = optim.SGD(net.parameters(), lr=_lr, momentum=0.9, weight_decay=5e-4)
+    optimizer = optim.SGD(net.parameters(), lr=_lr,
+                          momentum=0.9, weight_decay=5e-4)
     # optimizer = optim.Adam(net.parameters(), lr=_lr, weight_decay=5e-4)
 
     train_loss = 0
@@ -123,8 +144,9 @@ def train(epoch):
     total = 0
     now = time.time()
     # print(len())
-    for batch_idx, (inputs, targets) in enumerate(train_load):
 
+    for batch_idx, (inputs, targets) in enumerate(train_load):
+		
         inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad()
         outputs = net(inputs)
@@ -136,24 +158,27 @@ def train(epoch):
         _, predicted = outputs.max(1)
         total += targets.size(0)
         correct += predicted.eq(targets).sum().item()
-        if (batch_idx + 1)%20 == 0:
+        if (batch_idx + 1) % 20 == 0:
 
             loss_now = (train_loss / (batch_idx + 1))
             time_now = time.time()-now
             total_data = len(train_data)
             done_data = batch_idx * batch_size
-            print("Data: {} / {} Loss:{:.7f}  Time:{:.2f} s".format(done_data, total_data, loss_now, time_now))
+            print("Data: {} / {} Loss:{:.7f}  Time:{:.2f} s".format(done_data,
+                                                                    total_data, loss_now, time_now))
             now = time.time()
+
 
 def test(epoch):
 
     global best_acc, link_temp
+    device = "cuda"
     net.eval()
     test_loss = 0
     correct = 0
     total = 0
     with torch.no_grad():
-        for batch_idx, (inputs, targets) in enumerate(test_load):
+        for batch_idx, (inputs, targets) in enumerate(tqdm(test_load)):
             inputs, targets = inputs.to(device), targets.to(device)
 
             outputs = net(inputs)
@@ -181,24 +206,65 @@ def test(epoch):
         }
         if not os.path.isdir('checkpoint'):
             os.mkdir('checkpoint')
-    
+
         torch.save(state, link_temp)
         best_acc = acc
 
+def test1(epoch):
+    global real_best_acc, real_link_temp
+    device = "cuda"
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(tqdm(real_test_load)):
+            inputs, targets = inputs.to(device), targets.to(device)
+
+            outputs = net(inputs)
+            loss = F.cross_entropy(outputs, targets)
+
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+            # print("Batch: {}/{} Loss:{0.4f}".format(batch_idx, len(testloader), (test_loss / (batch_idx + 1))))
+            # progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)' % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+    # Save checkpoint.
+    # print(correct)
+    # print(total)
+    acc = (100 * correct) / total
+    print("Accuracy: {:.3f}%".format(acc))
+    if acc > real_best_acc:
+        print('Saving..')
+        print('Best accuracy real test: {:.3f}%'.format(acc))
+        state = {
+            'net': net.state_dict(),
+            'acc': acc,
+        }
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+
+        torch.save(state, real_link_temp)
+        real_best_acc = acc
+
 def main():
 
-    print("Start epoch: ",start_epoch)
+    print("Start epoch: ", start_epoch)
+    print("Total_epoch: ", total_epoch)
     print("Best now: ", best_acc)
-    print("Batch size: ",batch_size)
-    # pass
-    # for idx in range(start_epoch, total_epoch):
-    #     train(idx)
-    #     test(idx)
+    print("Batch size: ", batch_size)
+    # ultra-test
+    for idx in range(start_epoch, total_epoch):
+        train(idx)
+        test(idx)
+        test1(idx)
 
 if __name__ == "__main__":
     # pass
     # print(os.path.isfile('./checkpoint/resnet18.pt'))
     # print(">>> done <<<")
-    # print(">>> hey <<<")
-    # main()
-    # print(">>> done <<<")
+    print(">>> hey <<<")
+    main()
+    print(">>> done <<<")
